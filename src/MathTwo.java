27e5f8b1e23553a18106424d1d2c5b81fd606c51
eyasuh <eyha12@gmail.com>
2020-04-22 08:45:56 -0400

    first commit

---
+++ src/MathTwo.java
@@ -0,0 +1,7 @@
+public class MathTwo {
+    public static void main (String [] atgs) {
+        int myNumber = 2;
+        double myOtherNumber = 1.7938;
+        System.out.println("myNumber: " + myNumber + "\nmyOtherNumber: " + myOtherNumber);
+    }
+}
